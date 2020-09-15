468c8976c3eae2aaf9a7c5f981a2d60eeb38f456
ripalpatel1451 <ripalpatel1451@gmail.com>
2020-09-15 01:30:08 -0400

    2nd Commit...

--- restful-web-services/src/main/java/com/ripal/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/ripal/rest/webservices/restfulwebservices/user/User.java
@@ -3,13 +3,21 @@ package com.ripal.rest.webservices.restfulwebservices.user;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @ApiModel(description = "All details about the user.")
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "Name should have atleast 2 character")
@@ -20,6 +28,9 @@ public class User {
     @ApiModelProperty(notes = "Birthdate should be in the past")
     private Date birthDate;
 
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
     protected User() {
     }
 
@@ -27,6 +38,14 @@ public class User {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     public Integer getId() {

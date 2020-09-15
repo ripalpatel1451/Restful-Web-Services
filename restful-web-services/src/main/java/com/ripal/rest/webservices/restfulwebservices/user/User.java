7d90f93a5583aca91d3fe54210bfaa4c072a68ee
ripalpatel1451 <ripalpatel1451@gmail.com>
2020-09-15 01:55:52 -0400

    Revert "2nd Commit..."
    This reverts commit 468c8976

--- restful-web-services/src/main/java/com/ripal/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/ripal/rest/webservices/restfulwebservices/user/User.java
@@ -3,21 +3,13 @@ package com.ripal.rest.webservices.restfulwebservices.user;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
-import java.util.List;
 
 @ApiModel(description = "All details about the user.")
-@Entity
 public class User {
 
-    @Id
-    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "Name should have atleast 2 character")
@@ -28,9 +20,6 @@ public class User {
     @ApiModelProperty(notes = "Birthdate should be in the past")
     private Date birthDate;
 
-    @OneToMany(mappedBy = "user")
-    private List<Post> posts;
-
     protected User() {
     }
 
@@ -38,14 +27,6 @@ public class User {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
-    }
-
-    public List<Post> getPosts() {
-        return posts;
-    }
-
-    public void setPosts(List<Post> posts) {
-        this.posts = posts;
     }
 
     public Integer getId() {
--- restful-web-services/src/main/java/com/ripal/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ restful-web-services/src/main/java/com/ripal/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 
-    //Versioning using "URI based versioning"    ex- Twitter
     @RequestMapping(method = RequestMethod.GET, path = "v1/person")
     public  PersonV1 personV1(){
         return new PersonV1("Bob Charlie");
@@ -15,39 +14,6 @@ public class PersonVersioningController {
 
     @RequestMapping(method = RequestMethod.GET, path = "v2/person")
     public  PersonV2 personV2(){
-        return new PersonV2(new Name("Bob","Charlie"));
-    }
-
-    //Versioning using "request Param"   ex- Amazon
-    @RequestMapping(method = RequestMethod.GET, value = "/person/param", params = "version=1")
-    public  PersonV1 paramV1(){
-        return new PersonV1("Bob Charlie");
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/person/param", params = "version=2")
-    public  PersonV2 paramV2(){
-        return new PersonV2(new Name("Bob","Charlie"));
-    }
-
-    //Versioning using "header param" or "header versioning"    ex - Microsoft
-    @RequestMapping(method = RequestMethod.GET, value = "/person/header", headers = "X-API-VERSION=1")
-    public  PersonV1 headerV1(){
-        return new PersonV1("Bob Charlie");
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/person/header", headers = "X-API-VERSION=2")
-    public  PersonV2 headerV2(){
-        return new PersonV2(new Name("Bob","Charlie"));
-    }
-
-    //Versioning using "produces" or "Accept header versioning" or "MIME type versioning"       ex - GitHub
-    @RequestMapping(method = RequestMethod.GET, value = "/person/produces", produces = "application/vnd.company.app-v1+json")
-    public  PersonV1 producesV1(){
-        return new PersonV1("Bob Charlie");
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/person/produces", produces = "application/vnd.company.app-v2+json")
-    public  PersonV2 producesV2(){
         return new PersonV2(new Name("Bob","Charlie"));
     }
 }
